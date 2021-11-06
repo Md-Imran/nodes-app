8e09fafa981e3be61a80fb6bea89f203385acdaf
Imran11 <Annex@110211@>
2021-11-06 21:09:23 +0600

    yargs npm implementation

--- app.js
+++ app.js
@@ -3,9 +3,61 @@ const sum=add(5,6)
 console.log(sum) */
 
 const validator=require('validator')
-//import validator from 'validator'
+const yargs= require('yargs')
 
-const getNotes = require('./node')
+const chalk = require('chalk');
 
-console.log(validator.isEmail('raze.dsf@gamil.com'))
-console.log(validator.isURL('https://raze.com'))
+
+//console.log(validator.isEmail('raze.dsf@gamil.com'))
+//console.log(chalk.italic.red('Hello'))
+
+yargs.version('1.1.0')
+
+// add command 
+yargs.command({
+
+    command : 'add',
+    describe: 'Add a new note',
+    handler: function(){
+        console.log('Adding a new note') 
+    }
+
+})
+// remove command
+yargs.command({
+
+    command : 'remove',
+    describe: 'remove a new note',
+    handler: function(){
+        console.log('Remove a new note') 
+    }
+
+})
+
+
+yargs.command({
+
+    command : 'list',
+    describe: 'list command',
+    handler: function(){
+        console.log('Show the list') 
+    }
+
+})
+
+
+yargs.command({
+
+    command : 'read',
+    describe: 'read a value',
+    handler: function(){
+        console.log('Read the  value from server') 
+    }
+
+})
+
+console.log(yargs.argv)
+/* if(command==='add')
+console.log('Adding note!')
+else if (command== 'remove')
+console.log('Remove Note') */
